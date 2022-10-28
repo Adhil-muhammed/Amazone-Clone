62572c7e0272c11d366cf05d8de760bd0607b065
bootsrap <adhiltajudeen@gmail.com>
2022-10-28 12:54:34 +0530

    product added to cart

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useContext } from "react";
+import { DispactchContext } from "../../Context/StateProvider";
 import "./Products.css";
 function Products({ id, title, price, rating, image }) {
-  // var imge
+  const dispactch = useContext(DispactchContext);
+  const AddBasket = () => {
+    dispactch({
+      type: "ADD_TO_CART",
+      Payload: {
+        Id: id,
+        Tittle: title,
+        Price: price,
+        Images: image,
+        Rating: rating,
+      },
+    });
+  };
+
   return (
     <React.Fragment>
       <div key={id}>
-
-      <div className="product"  >
-        <div className="product__info">
-          <p>{title}</p>
-          <p className="product__price">
-            <small>$</small>
-            <strong>{price}</strong>
-          </p>
-          <div className="product__rating">
-            {Array(rating)
-              .fill()
-              .map((_, i) => (
-             <span>  <p>🌟</p></span> 
-              ))}
+        <div className="product">
+          <div className="product__info">
+            <p>{title}</p>
+            <p className="product__price">
+              <small>$</small>
+              <strong>{price}</strong>
+            </p>
+            <div className="product__rating">
+              {Array(rating)
+                .fill()
+                .map((_, i) => (
+                  <span>
+                    {" "}
+                    <p>🌟</p>
+                  </span>
+                ))}
+            </div>
           </div>
+
+          <img src={image} alt="" />
+
+          <button onClick={AddBasket}>Add to Basket</button>
         </div>
-
-        <img src={image} alt="" />
-
-        <button>Add to Basket</button>
-      </div>
       </div>
     </React.Fragment>
   );
--- src/Context/StateProvider.js
+++ src/Context/StateProvider.js
@@ -1,41 +1,38 @@
-import React, { useReducer,createContext } from "react";
+import React, { useReducer, createContext } from "react";
 
- const DispactchContext=createContext()
- const StateContext=createContext()
+const DispactchContext = createContext();
+const StateContext = createContext();
 
-
-
-
-
-
-
-
-function StateProvider({children}) {
-    console.log("this are my children",children);
-    const initialstate={
-        cartItems:[]
-    }
+const initialstate = {
+  cartItems: [],
+};
+function StateProvider({ children }) {
+  console.log("this are my children", children);
   //   this the reducer function
   const reducer = (state, action) => {
-    // switch (action) {
-    //   case value: //action needed
+    
+    
+    console.log(action);
+    switch (action.type) {
+      case "ADD_TO_CART":
+        return {
+          ...state,
+          cartItems: [...state.cartItems,action.Payload],
+        };
 
-    //   default:
-
-    //     return state;
-    // }
+      default:
+        return state;
+    }
   };
   const [state, dispactch] = useReducer(reducer, initialstate);
-
+  console.log('this is new state' , state)
   return (
-  <React.Fragment>
-    <DispactchContext.Provider value={dispactch}>
-        <StateContext.Provider value={state}>
-{children}
-        </StateContext.Provider>
-    </DispactchContext.Provider>
-  </React.Fragment>
-  )
+    <React.Fragment>
+      <DispactchContext.Provider value={dispactch}>
+        <StateContext.Provider value={state}>{children}</StateContext.Provider>
+      </DispactchContext.Provider>
+    </React.Fragment>
+  );
 }
 
-export { StateProvider,DispactchContext,StateContext}
+export { StateProvider, DispactchContext, StateContext };
